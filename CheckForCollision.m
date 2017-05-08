e148c52bb0fda393f9e48dab7cb7f2757112b30b
brucemingxinliu <mxl592@case.edu>
2017-05-08 10:18:11 -0400

    Update and rename checkForCollision.m to CheckForCollision.m

--- CheckForCollision.m
+++ CheckForCollision.m
@@ -1,14 +1,12 @@
-function res = checkForCollision(node1,node2)
-% res = 1 if no collision
-% res = 0 if collision
+function FLAG = CheckForCollision(node1,node2)
+% FLAG = 1 if no collision
+% FLAG = 0 if collision
 global map;
 
 res = 1;
 
 mapSize = size(map,2);
 
-
-% check for clockwise polygon
 tf2 = ispolycw(node1(:,1),node1(:,2));
 if ~tf2
     [a,b] = poly2cw(node1(:,1),node1(:,2));
@@ -24,7 +22,6 @@ end
 for i = 1:mapSize-1
    obs = map{i};
    
-   % check clockwise obstacle
     tf1 = ispolycw(obs(:,1),obs(:,2));
     if ~tf1
         [a,b] = poly2cw(obs(:,1),obs(:,2));
@@ -34,18 +31,18 @@ for i = 1:mapSize-1
    [x1,y1] = polybool('intersection',node1(:,1),node1(:,2),obs(:,1),obs(:,2));
    
    if isempty(x1)
-       res = res*1;
+       Flag = 1;
        
        [x2,y2] = polybool('intersection',node2(:,1),node2(:,2),obs(:,1),obs(:,2));
        
        if isempty(x2)
-           res = res*1;
+           Flag = 1;
        else
-           res = res*0;
+           Flag = 0;
        end
    
    else
-       res = res*0;
+       Flag = 0;
    end
    
    
